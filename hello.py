2b670bf09ef0bdb4405520808e88ebbe4ccc2c69
Santiago Arocha <arochas846@gmail.com>
2023-09-23 00:01:39 -0300

    some methods

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 name = "santi";
-print("Hello World: " , name);
-print (2+10);
+# print("Hello World: " , name);
+# print (2+10);
+
+# print("String object structure: -> ", dir(name));
+
+# print("String length: -> ", len(name));
+
+# print("Checking lowercase boolean: -> ", name.islower());
+
+# print("Checking uppercase boolean: -> ", name.isupper());
+
+print("Replacing a letter: -> ", name.replace('n','m'));
