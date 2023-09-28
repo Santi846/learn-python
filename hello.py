e67caa6d5aa69bab0154bbc72876a897c9cc29ce
Santiago Arocha <arochas846@gmail.com>
2023-09-27 23:45:05 -0300

    Modules

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
+#import script
+#adding an alias
+import mymodule as m;
+
+#call import file fucntion
+m.my_fucntion();
 # name = "santi";
 # print("Hello World: " , name);
 # print (2+10);
 
 # print("String object structure: -> ", dir(name));
+
+print("Import object structure: -> ", dir(m));
 
 # print("String length: -> ", len(name));
 
