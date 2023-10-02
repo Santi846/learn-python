c87a4a842fd55fca5ed964071a3b80d9b178854a
Santiago Arocha <arochas846@gmail.com>
2023-10-01 22:12:33 -0300

    Floating data type

--- data_types.py
+++ data_types.py
@@ -1,4 +1,5 @@
 import random;
+from math import floor, ceil;
 
 #karray -> list
 list = [1,2,3];
@@ -15,7 +16,7 @@ age_two = 21;
 # print(str(age_two));
 
 #float to integer
-float=2.3;
+# float_number=2.3;
 # print(int(float));
 
 #random number from a range
@@ -28,4 +29,31 @@ def verify(input):
     else:
         print('Its not an integer');
         
-verify(age_two);
+# verify(age_two);
+
+#floats
+f = 1.45;
+f2 = float(2.0);
+f3 = float("2");
+f4 = 1.45e3;
+#testing them
+# print(float(f4));
+
+#round characters at the right of the coma
+# print(round(f,2));
+
+#round the number to float down the nearest number
+# print(floor(f));
+
+#round the number to float up the nearest number
+# print(ceil(f));
+
+def verify_floats(number1, number2):
+    if (number1 == number2):
+        print('Float numbers are equal');
+    else:
+        print('Float numbers are different');
+        
+# verify_floats(f, f4);
+
+print(round((0.1 + 0.2), 2));
